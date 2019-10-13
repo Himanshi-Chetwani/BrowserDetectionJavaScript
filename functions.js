bbe6d3bcbc5c10fd40999991fc08d0e2aaebec9a
Himanshi Chetwani <himanshi.chetwani@gmail.com>
2019-10-13 11:11:27 -0400

    Phase 2

--- functions.js
+++ functions.js
@@ -1,32 +1,37 @@
 function createTxtNode(txtInput){
-    let txtEle=createTxtNode(txtInput);
+    let txtEle=document.createTextNode(txtInput);
     return txtEle;
 }
 function creatingHeader(actualJSON){
     let hEle=document.createElement("h2");
-    console.log(actualJSON);
     let txtInput=actualJSON.headerTexts["headerText1"];
-    console.log(txtInput);
     let txtEle=createTxtNode(txtInput);
     hEle.appendChild(txtEle);
     return hEle;
 }
+function createOptions(actualJSON){
+   
+
+}
 function createSelectBlock(actualJSON){
     let hEle=creatingHeader(actualJSON);
     document.body.appendChild(hEle);
+    let sEle=document.createElement("select");
+    let option1=actualJSON.optionsText["option1"][0];
+    let option1=actualJSON.optionsText["option1"][1];
+    let op1Ele=document.createElement("option");
+    createTxtNode(option1);
+    op1Ele.appendChild(option1);
+    hEle.appendChild(sEle);
 }
 function readingJSONFile(){
     const url = `https://raw.githubusercontent.com/Himanshi-Chetwani/Temp/master/data.json`;
-    let http = new XMLHttpRequest(),
-    stuffForPage="";
-    msgTag=document.getElementsByTagName("msg");
-    let returnVal;
+    let http = new XMLHttpRequest();
     http.open("GET", url, true);
     http.onreadystatechange = handleHttpResponse;
     function handleHttpResponse() {
         if (http.readyState === 4 && http.status === 200) {
-            console.log("1",http.responseText)
-            //callback(http.responseText);
+            callback(http.responseText);
         }
     };
     http.send(null);
@@ -36,15 +41,11 @@ function setCounter(){
     let counter=0;
 }
 function init(){
-    let retval=readingJSONFile();
-    /*callback=(response)=>{
-         actualJSON=JSON.parse(response);
-        console.log(actualJSON);
+    let retval=readingJSONFile(callback=(response)=>{
+        actualJSON=JSON.parse(response);
         createSelectBlock(actualJSON);
-        
-    });*/
-    
-}
+        });
+    }
 
 
 
