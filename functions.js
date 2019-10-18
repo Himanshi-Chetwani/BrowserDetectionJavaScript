7796c1dc16d511626ed2ca72bca38ce29e100732
Himanshi Chetwani <himanshi.chetwani@gmail.com>
2019-10-18 15:21:58 -0400

    Cookies added

--- functions.js
+++ functions.js
@@ -15,23 +15,22 @@ function creatingHeader(stringTxt) {
     hEle.appendChild(txtEle);
     return hEle;
 }
-function createChoices(){
-    let dEle=document.createElement("div");
-    dEle.setAttribute("id","ls");
-    let hEle=creatingHeader("Your chosen options are :");
-    let br=document.createElement("br");
+function createChoices() {
+    let dEle = document.createElement("div");
+    dEle.setAttribute("id", "ls");
+    let hEle = creatingHeader("Your chosen options are :");
+    let br = document.createElement("br");
     let tempS;
-    for(let i=0;i<localStorage.length-1;i++)
-    {
-        let lEle=document.createElement("li");
-        tempS="string"+i;
-        tempS=localStorage.getItem(tempS);
-        let txtEle=createTxtNode(tempS);
+    for (let i = 0; i < localStorage.length; i++) {
+        let lEle = document.createElement("li");
+        tempS = "string" + i;
+        tempS = localStorage.getItem(tempS);
+        let txtEle = createTxtNode(tempS);
         lEle.appendChild(txtEle);
         lEle.appendChild(txtEle);
         hEle.appendChild(lEle);
     }
-    
+
     dEle.appendChild(hEle);
     return dEle;
 }
@@ -41,26 +40,31 @@ function createSelectBlock(optionList) {
     counter = counter + 1;
     divEle.setAttribute("id", counter);
     let labelEle = document.createElement("label");
-    labelEle.setAttribute("for",counter+5);
+    labelEle.setAttribute("for", counter + 5);
     let txtEle = createTxtNode(Object.keys(optionList)[0]);
     labelEle.appendChild(txtEle);
     let sEle = document.createElement("select");
-    sEle.setAttribute("id",counter+5)
+    sEle.setAttribute("id", counter + 5)
     for (let i = 1; i < (Object.keys(optionList).length); i++) {
         optionTxt = Object.keys(optionList)[i];
         opEle = createOptions(optionTxt);
         sEle.appendChild(opEle);
     }
-    if (((Object.keys(optionList).length)==1)) {
+    if (((Object.keys(optionList).length) == 1)) {
         hEle1 = creatingHeader("Click here to unravel your study destination!");
-        linkEle=document.createElement("a");
-        let tempSt=Object.keys(optionList)[length];
-        linkEle.href=optionList[tempSt];
+        linkEle = document.createElement("a");
+        let tempSt = Object.keys(optionList)[length];
+        linkEle.href = optionList[tempSt];
         linkEle.setAttribute("id", "ans");
         linkEle.appendChild(hEle1);
         document.body.appendChild(linkEle);
         let fEle = createForm();
-        let chEle=createChoices();
+        let hfEle;
+        fEle.addEventListener("click",()=>{
+            console.log("Entering");
+            storeCookie();
+        })
+        let chEle = createChoices();
         linkEle.after(fEle);
         fEle.after(chEle);
         return;
@@ -84,13 +88,13 @@ function createSelectBlock(optionList) {
     divEle.appendChild(labelEle);
     document.body.appendChild(divEle);
     sEle.addEventListener("change", () => {
-        let tempString="string"+divEle.id;
-        localStorage.setItem(tempString,sEle.value);
+        let tempString = "string" + divEle.id;
+        localStorage.setItem(tempString, sEle.value);
         if (prev >= divEle.id) {
             if (document.getElementById("formDiv") != null) {
                 document.getElementById("formDiv").remove();
             }
-            for (let i = parseInt(divEle.id) + 1; i < ((Object.keys(optionList).length)-1); i++) {
+            for (let i = parseInt(divEle.id) + 1; i < ((Object.keys(optionList).length) - 1); i++) {
                 if (document.getElementById(i) != null) {
                     document.getElementById(i).remove();
                     prev = counter;
@@ -99,9 +103,6 @@ function createSelectBlock(optionList) {
                 if (document.getElementById("ans") != null) {
                     document.getElementById("ans").remove();
                 }
-                /*if (document.getElementById("ans2") != null) {
-                    document.getElementById("ans2").remove();
-                }*/
                 if (document.getElementById("ls") != null) {
                     document.getElementById("ls").remove();
                 }
@@ -115,7 +116,19 @@ function createSelectBlock(optionList) {
     });
 }
 function readingJSONFile() {
-    const url = `https://raw.githubusercontent.com/Himanshi-Chetwani/Temp/master/data_new.json`;
+    let url='https://raw.githubusercontent.com/Himanshi-Chetwani/Temp/master/data_new.json';
+    /*let togEle=document.createElement("button");
+    let togTele=createTxtNode("Toggle Data");
+    togEle.addEventListener("click",()=>{
+        if(url=='https://raw.githubusercontent.com/Himanshi-Chetwani/Temp/master/data_new.json'){
+            url='https://raw.githubusercontent.com/Himanshi-Chetwani/Temp/master/data_Set_two.json'
+        }
+        else{
+            url='https://raw.githubusercontent.com/Himanshi-Chetwani/Temp/master/data_new.json';
+        }
+    });
+   
+    */
     let http = new XMLHttpRequest();
     http.open("GET", url, true);
     http.onreadystatechange = handleHttpResponse;
@@ -126,7 +139,7 @@ function readingJSONFile() {
     };
     http.send(null);
 }
-function createSection(inp1) {
+function createSection(inp1) {  
     let uEle = document.createElement("ul");
     let lEle = document.createElement("li");
     let labelEle = document.createElement("label");
@@ -139,25 +152,60 @@ function createSection(inp1) {
         inpEle.setAttribute("type", "email");
         inpEle.setAttribute("pattern", "[A-Za-z0-9._%+-]+@rit.edu");
     }
-    /*if(inp1=="Name"){
-        inpEle.setAttribute("pattern","^[A-Z][a-z]+\s[A-Z][a-z]+$")
-    }*/
+    let txtNode;
+    if(inp1=="Name"){
+        let userName = cookies.getCookie("user_name");
+        if(!userName){
+            txtNode="New User";
+        } else {
+
+            txtNode=userName;
+        }
+        /*let userId = cookies.getCookie("userId");
+        if(!userId){
+            txtNode=creatingHeader("Welcome New User");
+            cookies.setCookie("userId",inpEle.value);
+            console.log(document.cookies);
+        }
+        else{
+            txtNode=creatingHeader("Hi "+userId);
+        }*/
+        //inpEle.setAttribute("pattern","^[A-Z][a-z]+\s[A-Z][a-z]+$")
+    }
     inpEle.setAttribute("placeholder", inp1);
     lEle.appendChild(labelEle);
     lEle.appendChild(inpEle);
     uEle.appendChild(lEle);
+    if(inp1=="Name"){
+        return [txtNode,uEle];
+    }
     return uEle;
 
+}
+function storeCookie(){
+    let personName = document.getElementById("Name").value
+    cookies.setCookie("user_name", personName)
 }
 function createForm() {
     let divEle = document.createElement("div");
     divEle.setAttribute("id", "formDiv");
-    let hEle = creatingHeader("Sending Your Results!");
     let fEle = document.createElement("form");
     fEle.setAttribute("class", "info-form");
     fEle.setAttribute("autocomple", "on");
     let inp1 = "Name";
-    let uEle1 = createSection(inp1);
+    let arr = createSection(inp1);
+    let hEle;
+    let uEle1;
+    if(arr.length==1){
+         hEle = creatingHeader("Sending Your Results!");
+         uEle1=arr;
+    }
+    else{
+        let str="Sending Your Results "
+        let temp=str.concat(arr[0]);
+        hEle=creatingHeader(temp);
+        uEle1=arr[1];
+    }
     let bEle = document.createElement("button");
     bEle.setAttribute("type", "submit");
     bEle.setAttribute("name", "submit");
